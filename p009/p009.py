f81bbb42119847e7cd0cc6e4a43b85c05d9dc1bf
masaponto <masaponto@gmail.com>
2015-11-07 13:52:24 +0900

    [FIX] applyed pep8

--- p009/p009.py
+++ p009/p009.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def solve() -> int:
-    for i in range(1,1000 - 2):
+    for i in range(1, 1000 - 2):
         for j in range(i + 1, 1000 - 2 - i):
             for k in range(j + 1, 1000 - 2 - j):
                 if (i**2 + j**2 == k**2 and i + j + k == 1000):
-                    return i*j*k
+                    return i * j * k
     return 0
-
 
 
 def main():
