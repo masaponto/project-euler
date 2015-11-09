23eaeebc1283ac8c2180d8ae9c5182e378b4c097
masaponto <masaponto@gmail.com>
2015-11-09 14:11:58 +0900

    [FIX] this program need much time

--- p010/p010.py
+++ p010/p010.py
@@ -1,32 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
-
-
-def get_prime(n):
-    plist = []
-    slist = list(range(2, n))
-
-    while True:
-        m = slist.pop(0)
-        plist.append(m)
-
-        for i in slist:
-            if i % m == 0:
-                slist.remove(i)
-
-        if len(slist) == 0 or slist[0] == int(math.sqrt(m) + 0.5):
-            break
-
-    return plist
-
-
 def solve(n):
-    return sum(get_prime(n))
-
-
-def solve_(n):
     plist = [2]
     m = 3
     while m < n:
@@ -44,23 +19,9 @@ def solve_(n):
     return sum(plist)
 
 
-def hoge(n):
-    slist = list(range(3, n, 2))
-
-    nplist = []
-    for m in slist:
-        for j in range(2, m):
-            if m % j == 0:
-                nplist.append(m)
-                break
-
-    return 2 + sum(slist) - sum(nplist)
-
-
 def main():
-    # print(hoge(10))
-    #print(hoge(2000000))
-    print(solve_(2000000))
+    print(solve(10))
+    print(solve(2000000))
 
 if __name__ == "__main__":
     main()
