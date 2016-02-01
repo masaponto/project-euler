3f42c73d91b3e52dd28f410f595e33e3178d7f15
masaponto <masaponto@gmail.com>
2016-02-01 19:58:16 +0900

    [FIX]

--- p021/p021.py
+++ p021/p021.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from sympy.ntheory import factorint
+from functools import reduce
 
+
+def mat(lst):
+    return reduce(lambda x, y: x*y, lst, 1)
 
 def d(n):
-    factors = factorint(n)
-    s = 1
-    for p, i in factors.items():
-        s *= sum([p**n for n in range(i + 1)])
-    return s - n
-
+    return mat([sum([p**n for n in range(i + 1)]) for p, i in factorint(n).items()]) - n
 
 def solve():
     s = [n for n in range(2, 10000) if n == d(d(n)) and n != d(n)]
